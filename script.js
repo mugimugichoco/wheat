93dbbe77e7516411ea82e0d27ec831fd5395e417
mugimugichoco <67958183+mugimugichoco@users.noreply.github.com>
2020-08-08 05:26:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-$(function(){
-
+$(function() {
+  $('#login-show').click(function() {
+    $('#login-modal').fadeIn();
+  });
+  
+  $('.signup-show').click(function() {
+    $('#signup-modal').fadeIn();
+  });
+  
+  // 「.close-modal」要素にclickイベントを設定してください
+  $('.close-modal').click(function() {
+    $('#login-modal').fadeOut();
+    $('#signup-modal').fadeOut();
+  });
+  
 });
